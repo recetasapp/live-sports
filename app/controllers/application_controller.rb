3c5a20c9eb324ede18c87b6a522942f7fbb7e19c
Julian <microweb10@gmail.com>
2014-10-26 08:32:48 +0100

    add view and controller for list of sports

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from NetworkException, with: :not_found
+
+  protected
+  def not_found(exception)
+    redirect_to root_path, alert: exception.cause
+  end
 end
